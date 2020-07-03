5d2addf673df130bfbcb5e4c92f976dc1279e83b
jleflang@ucmerced.edu <jleflang@ucmerced.edu>
2020-07-03 16:20:31 -0700

    A4: add comments

--- A4.js
+++ A4.js
@@ -6,11 +6,20 @@
 
 
 function buildList(list) {
+    // Local var
     var result = [];
+
+    // Let's bind the console to insure function in cli node
     var newLog = console.log.bind(console)
+
     for (var i = 0; i < list.length; i++) {
+        // To loop correctly, make a closure out of i and includes the args of
+        //  the enclosed code
         ( function(i) {
+            // Create an item
             var item = 'item' + list[i];
+
+            // Push to list
             result.push( function() {newLog(item + ' ' + list[i])} );
         })(i);
     }
@@ -18,6 +27,7 @@ function buildList(list) {
 }
  
 function testList() {
+    // Example list
     var fnlist = buildList([1,2,3]);
     // using j only to help prevent confusion - could use i
     for (var j = 0; j < fnlist.length; j++) {
@@ -26,4 +36,5 @@ function testList() {
 
 }
 
+// Test it!
 testList();
