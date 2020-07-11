71694ec92fc75692392b33079b848df81a2c396d
jleflang@ucmerced.edu <jleflang@ucmerced.edu>
2020-07-10 22:56:51 -0700

    A5: Consolidate and improve the js

--- A5/A5.js
+++ A5/A5.js
@@ -3,13 +3,22 @@ var apiKey = 'b63aa6f556f33f2023cfbef3a6bca38f';
 document.addEventListener('DOMContentLoaded', bindButtons);
 
 function bindButtons(){
-    document.getElementById('submitCity').addEventListener('click', function(event){
+    document.getElementById('submitWeather').addEventListener('click', function(event){
         var req = new XMLHttpRequest();
-        var payload = {cityGet:null};
 
-        payload.cityGet = document.getElementById('cityGet').value;
+        let input = document.getElementById('locGet').value;
+        // https://www.w3schools.com/jsref/jsref_split.asp 
+        // using to split the string to great effect
+        let res = input.split(',');
 
-        req.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=' + payload.cityGet + '&units=imperial&appid=' + apiKey, true);
+        var loc = res[0];
+        var country = res[1];
+
+        if (typeof loc == 'number') {
+            req.open('GET', 'https://api.openweathermap.org/data/2.5/weather?zip=' + loc + ',' + country + '&units=imperial&appid=' + apiKey, true);
+        } else {
+            req.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=' + loc + ',' + country + '&units=imperial&appid=' + apiKey, true);
+        }
         req.send(null);
 
         req.addEventListener('load',function(){
@@ -25,27 +34,6 @@ function bindButtons(){
         event.preventDefault();
     })
 
-    document.getElementById('submitZip').addEventListener('click', function(event){
-        var req = new XMLHttpRequest();
-        var payload = {zipGet:null};
-
-        payload.zipGet = document.getElementById('zipGet').value; 
-        req.open('GET', 'https://api.openweathermap.org/data/2.5/weather?zip=' + payload.zipGet + '&units=imperial&appid=' + apiKey, true);
-        req.send(null);
-
-        req.addEventListener('load',function(){
-            if(req.status >= 200 && req.status < 400){
-                var response = JSON.parse(req.responseText);
-                document.getElementById('locRequest').textContent = response.name + ', ' + response.sys.country;
-                document.getElementById('reply').textContent = response.weather[0].main + ', Temperature: ' + response.main.temp + '\xB0F, Wind: ' + 
-                response.wind.speed + ' miles/hour @ ' + response.wind.deg + '\xB0';
-            } else {
-                console.log("Error in network request: " + req.statusText);
-            }
-        });
-        event.preventDefault();
-    })
-    
     document.getElementById('submitHttpBin').addEventListener('click', function(event){
         var req = new XMLHttpRequest();
         var payload = null;
