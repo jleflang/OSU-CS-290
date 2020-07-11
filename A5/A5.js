e915922274336f4688272647090827399e7278a8
jleflang@ucmerced.edu <jleflang@ucmerced.edu>
2020-07-10 20:47:22 -0700

    A5: Forms and DOM with the Openweathermap API

---
+++ A5/A5.js
@@ -0,0 +1,42 @@
+var apiKey = 'b63aa6f556f33f2023cfbef3a6bca38f';
+
+document.addEventListener('DOMContentLoaded', bindButtons);
+
+function bindButtons(){
+    document.getElementById('submitCity').addEventListener('click', function(event){
+        var req = new XMLHttpRequest();
+        var payload = {cityGet:null};
+        payload.cityGet = document.getElementById('cityGet').value;
+        req.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=' + payload.cityGet + '&units=imperial&appid=' + apiKey, true);
+        req.send(null);
+        req.addEventListener('load',function(){
+            if(req.status >= 200 && req.status < 400){
+                var response = JSON.parse(req.responseText);
+                document.getElementById('locRequest').textContent = response.name + ', ' + response.sys.country;
+                document.getElementById('reply').textContent = response.weather[0].main + ', Temperature: ' + response.main.temp + '\xB0F, Wind: ' + 
+                response.wind.speed + ' miles/hour @ ' + response.wind.deg + '\xB0';
+            } else {
+                console.log("Error in network request: " + req.statusText);
+            }
+        });
+        event.preventDefault();
+    })
+    document.getElementById('submitZip').addEventListener('click', function(event){
+        var req = new XMLHttpRequest();
+        var payload = {zipGet:null};
+        payload.zipGet = document.getElementById('zipGet').value; 
+        req.open('GET', 'https://api.openweathermap.org/data/2.5/weather?zip=' + payload.zipGet + '&units=imperial&appid=' + apiKey, true);
+        req.send(null);
+        req.addEventListener('load',function(){
+            if(req.status >= 200 && req.status < 400){
+                var response = JSON.parse(req.responseText);
+                document.getElementById('locRequest').textContent = response.name + ', ' + response.sys.country;
+                document.getElementById('reply').textContent = response.weather[0].main + ', Temperature: ' + response.main.temp + '\xB0F, Wind: ' + 
+                response.wind.speed + ' miles/hour @ ' + response.wind.deg + '\xB0';
+            } else {
+                console.log("Error in network request: " + req.statusText);
+            }
+        });
+        event.preventDefault();
+    })
+}
